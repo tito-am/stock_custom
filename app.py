e5fd2c40d825942deeb2b54c3d17e219afa0209c
tito arandia <fabian.arandia@gmail.com>
2022-10-23 16:11:03 -0400

    projet de nate

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask
+import git
+from src.business_logic.process_query import create_business_logic
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def hello():
+    return f'Hello dear students, you should use a better route:!\nEX: get_stock_val/<ticker>\n'
+
+
+@app.route('/get_stock_val/<ticker>', methods=['GET'])
+def get_stock_value(ticker):
+    bl = create_business_logic()
+    prediction = bl.do_predictions_for(ticker)
+
+    return f'{prediction}\n'
+
+
+@app.route('/getversion/')
+def getversion():
+    repo = git.Repo(search_parent_directories=True)
+    sha = repo.head.object.hexsha
+
+    return f'{sha}\n'
+
+
+if __name__ == '__main__':
+    # Used when running locally only. When deploying to Cloud Run,
+    # a webserver process such as Gunicorn will serve the app.
+    app.run(host='localhost', port=8080, debug=True)
